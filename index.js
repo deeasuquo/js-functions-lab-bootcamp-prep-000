19f02e022af780465c00214edf1832923737cbc8
deeasuquo@gmail.com <deeasuquo@gmail.com>
2018-08-10 17:23:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function happyHolidaysTo(name) {
   return string = "Happy holidays, you!"
 }
 function happyHolidaysTo(celebration,name) {
-  return string = "Happy easter, Dorcas!"
+  return string = "Happy easters, you!"
 }
